143d7b9358a6b35459d5e6e5b9f2ab788f10f8ae
EugenAlexandrov <mybclym@gmail.com>
2022-09-07 05:45:06 +0300

    add sight images

---
+++ lib/domain/model/place_image.dart
@@ -0,0 +1,6 @@
+class PlaceImage {
+  final String? url;
+  final int color;
+
+  PlaceImage({this.url, required this.color});
+}
--- lib/ui/screens/add_sight_screen.dart/add_sight_screen.dart
+++ lib/ui/screens/add_sight_screen.dart/add_sight_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:places/app_router.dart';
 import 'package:places/app_strings.dart';
 import 'package:places/colors.dart';
+import 'package:places/domain/model/place_image.dart';
 import 'package:places/domain/model/sight.dart';
 import 'package:places/domain/repository/filter_repository.dart';
 import 'package:places/domain/repository/sight_repository.dart';
@@ -34,17 +35,10 @@ class _NewSightScreenState extends State<NewSightScreen> {
   final descriptionController = TextEditingController();
 
   int? filterId;
+  // local model for images persist
+  final ImageListModel _model = ImageListModel();
 
   final decoration = InputDecoration(
-    // suffixIcon: IconButton(
-    //   color: AppColors.lmGreen,
-    //   icon: const Icon(
-    //     Icons.cancel,
-    //   ),
-    //   onPressed: () {
-    //     print('suffix tap');
-    //   },
-    // ),
     contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
     enabledBorder: OutlineInputBorder(
       borderSide: const BorderSide(color: AppColors.lmGreen, width: 1),
@@ -67,9 +61,7 @@ class _NewSightScreenState extends State<NewSightScreen> {
         backgroundColor: Colors.transparent,
         title: Text(
           AppStrings.addSightAppbarTitle,
-          style: theme
-              .textTheme
-              .headline6
+          style: theme.textTheme.headline6
               ?.copyWith(color: theme.colorScheme.lmMainDmWhite),
         ),
         centerTitle: true,
@@ -79,126 +71,124 @@ class _NewSightScreenState extends State<NewSightScreen> {
           },
           child: Text(
             AppStrings.cancel,
-            style: theme.textTheme.bodyText1?.copyWith(
-                color:theme.colorScheme.smallSecondaryTwo),
+            style: theme.textTheme.bodyText1
+                ?.copyWith(color: theme.colorScheme.smallSecondaryTwo),
           ),
         ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              margin: const EdgeInsets.only(top: 8),
-              height: 72,
-              alignment: Alignment.center,
-              color: Colors.amber,
-              child: const Text('Galary'),
-            ),
-            MyInputField(
-              title: AppStrings.addSightCategories,
-              child: ListTile(
-                contentPadding: const EdgeInsets.all(0),
-                onTap: () {
-                  _navigateAndPickFIlterType(context);
-                },
-                title: Text(filterId == null
-                    ? AppStrings.addSightNoFiltersSelect
-                    : Provider.of<FilterRepository>(context)
-                        .getFilterById(filterId!)
-                        .title),
-                trailing: SvgPicture.asset(AssetImages.iconRihgtArrowPath),
+        // решил поэкспериментировать с инхеритами
+        child: ImageListProvider(
+          model: _model,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Gallary(),
+              MyInputField(
+                title: AppStrings.addSightCategories,
+                child: ListTile(
+                  contentPadding: const EdgeInsets.all(0),
+                  onTap: () {
+                    _navigateAndPickFIlterType(context);
+                  },
+                  title: Text(filterId == null
+                      ? AppStrings.addSightNoFiltersSelect
+                      : Provider.of<FilterRepository>(context)
+                          .getFilterById(filterId!)
+                          .title),
+                  trailing: SvgPicture.asset(AssetImages.iconRihgtArrowPath),
+                ),
               ),
-            ),
-            const Divider(),
-            MyInputField(
-              title: AppStrings.addSightSightTitle,
-              child: TextField(
-                controller: titleController,
-                textAlign: TextAlign.start,
-                focusNode: titleFocus,
-                onSubmitted: (String value) {
-                  FocusScope.of(context).requestFocus(latitudeFocus);
-                },
-                textInputAction: TextInputAction.next,
-                style: theme.textTheme.bodyText1,
-                decoration: decoration,
+              const Divider(),
+              MyInputField(
+                title: AppStrings.addSightSightTitle,
+                child: TextField(
+                  controller: titleController,
+                  textAlign: TextAlign.start,
+                  focusNode: titleFocus,
+                  onSubmitted: (String value) {
+                    FocusScope.of(context).requestFocus(latitudeFocus);
+                  },
+                  textInputAction: TextInputAction.next,
+                  style: theme.textTheme.bodyText1,
+                  decoration: decoration,
+                ),
               ),
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Expanded(
-                  child: MyInputField(
-                    title: AppStrings.addSightLatitude,
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Expanded(
+                    child: MyInputField(
+                      title: AppStrings.addSightLatitude,
+                      child: TextField(
+                        controller: latitudeController,
+                        focusNode: latitudeFocus,
+                        onSubmitted: (String value) {
+                          FocusScope.of(context).requestFocus(longitudeFocus);
+                        },
+                        keyboardType: const TextInputType.numberWithOptions(),
+                        textInputAction: TextInputAction.next,
+                        decoration: decoration,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: MyInputField(
+                      title: AppStrings.addSightLongitude,
+                      child: TextField(
+                        controller: longitudeController,
+                        focusNode: longitudeFocus,
+                        onSubmitted: (String value) {
+                          FocusScope.of(context).requestFocus(descriptionFocus);
+                        },
+                        keyboardType: const TextInputType.numberWithOptions(),
+                        textInputAction: TextInputAction.next,
+                        decoration: decoration,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              TextButton(
+                  onPressed: () {},
+                  child: const Text(AppStrings.addSightShowOnMap),
+                  style: TextButton.styleFrom(padding: EdgeInsets.zero)),
+              Expanded(
+                child: MyInputField(
+                  title: AppStrings.addSightDescription,
+                  child: Expanded(
                     child: TextField(
-                      controller: latitudeController,
-                      focusNode: latitudeFocus,
-                      onSubmitted: (String value) {
-                        FocusScope.of(context).requestFocus(longitudeFocus);
-                      },
-                      keyboardType: const TextInputType.numberWithOptions(),
-                      textInputAction: TextInputAction.next,
-                      decoration: decoration,
+                      textAlign: TextAlign.justify,
+                      controller: descriptionController,
+                      focusNode: descriptionFocus,
+                      maxLines: null,
+                      expands: true,
+                      textInputAction: TextInputAction.done,
+                      decoration: decoration.copyWith(),
                     ),
                   ),
                 ),
-                const SizedBox(width: 16),
-                Expanded(
-                  child: MyInputField(
-                    title: AppStrings.addSightLongitude,
-                    child: TextField(
-                      controller: longitudeController,
-                      focusNode: longitudeFocus,
-                      onSubmitted: (String value) {
-                        FocusScope.of(context).requestFocus(descriptionFocus);
-                      },
-                      keyboardType: const TextInputType.numberWithOptions(),
-                      textInputAction: TextInputAction.next,
-                      decoration: decoration,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            TextButton(
-              onPressed: () {},
-              child: const Text(AppStrings.addSightShowOnMap),
-            ),
-            Expanded(
-              child: MyInputField(
-                title: AppStrings.addSightDescription,
-                child: Expanded(
-                  child: TextField(
-                    textAlign: TextAlign.justify,
-                    controller: descriptionController,
-                    focusNode: descriptionFocus,
-                    maxLines: null,
-                    expands: true,
-                    textInputAction: TextInputAction.done,
-                    decoration: decoration.copyWith(),
-                  ),
-                ),
               ),
-            ),
-            const SizedBox(height: 28),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: (titleController.text.isEmpty ||
-                        longitudeController.text.isEmpty ||
-                        latitudeController.text.isEmpty ||
-                        descriptionController.text.isEmpty ||
-                        filterId == null)
-                    ? null
-                    : () {
-                        addSight(context);
-                      },
-                child: const Text(AppStrings.addSightCreate),
-              ),
-            )
-          ],
+              const SizedBox(height: 28),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: (titleController.text.isEmpty ||
+                          longitudeController.text.isEmpty ||
+                          latitudeController.text.isEmpty ||
+                          descriptionController.text.isEmpty ||
+                          filterId == null)
+                      ? null
+                      : () {
+                          addSight(context);
+                        },
+                  child: const Text(AppStrings.addSightCreate),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -210,7 +200,8 @@ class _NewSightScreenState extends State<NewSightScreen> {
         lon: double.parse(longitudeController.text),
         lat: double.parse(latitudeController.text),
         details: descriptionController.text,
-        filterId: filterId!);
+        filterId: filterId!,
+        images: ImageListProvider.read(context)?.imageList ?? <PlaceImage>[]);
     Provider.of<SightRepository>(context, listen: false).addSight(newSight);
     Navigator.pop(context);
   }
@@ -221,5 +212,129 @@ class _NewSightScreenState extends State<NewSightScreen> {
     setState(() {
       filterId = result;
     });
+  }
+}
+
+class Gallary extends StatelessWidget {
+  const Gallary({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final model = ImageListProvider.watch(context);
+    return SizedBox(
+      height: 72,
+      child: ListView.separated(
+        scrollDirection: Axis.horizontal,
+        //смещение индексов для первого жлемента списка
+        itemCount: model == null ? 1 : model.imageList.length + 1,
+        itemBuilder: ((context, index) {
+          return index == 0
+              ? InkWell(
+                  borderRadius: const BorderRadius.all(Radius.circular(12)),
+                  onTap: () {
+                    model?.addImage();
+                  },
+                  splashColor: AppColors.dmInnactiveBlack,
+                  child: Container(
+                    width: 72,
+                    child: const Icon(
+                      Icons.add_rounded,
+                      size: 48,
+                      color: Colors.green,
+                    ),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(color: Colors.green, width: 2)),
+                  ),
+                )
+              : ImageTile(index - 1);
+        }),
+        separatorBuilder: (BuildContext context, int index) {
+          return const SizedBox(
+            width: 16,
+          );
+        },
+      ),
+    );
+  }
+}
+
+// модель для InheritedNotifier
+class ImageListModel extends ChangeNotifier {
+  final List<PlaceImage> _imageList = [];
+
+  get imageList => List.castFrom(_imageList);
+
+  void addImage() {
+    imageList.add(PlaceImage(color: imageList.length));
+    notifyListeners();
+  }
+
+  void deleteImage(int index) {
+    imageList.removeAt(index);
+    notifyListeners();
+  }
+}
+
+//сам инхерит
+class ImageListProvider extends InheritedNotifier<ImageListModel> {
+  final ImageListModel model;
+
+  const ImageListProvider(
+      {required this.model, required Widget child, Key? key})
+      : super(key: key, notifier: model, child: child);
+
+  //подписка
+  static ImageListModel? watch(BuildContext context) {
+    return context
+        .dependOnInheritedWidgetOfExactType<ImageListProvider>()
+        ?.model;
+  }
+
+  //чтение
+  static ImageListModel? read(BuildContext context) {
+    final widget = context
+        .getElementForInheritedWidgetOfExactType<ImageListProvider>()
+        ?.widget;
+    return widget is ImageListProvider ? widget.notifier : null;
+  }
+}
+
+//Заглушка для изображений
+class ImageTile extends StatelessWidget {
+  const ImageTile(this.index, {Key? key}) : super(key: key);
+
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    final int color =
+        ImageListProvider.read(context)?.imageList[index].color ?? 10;
+    return Stack(
+      children: [
+        Container(
+          height: 72,
+          width: 72,
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(
+                color * 100 % 255, 255 - color * 10, color * 15, 1),
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+        Positioned(
+            top: 6,
+            right: 6,
+            child: GestureDetector(
+              onTap: () {
+                ImageListProvider.read(context)?.deleteImage(index);
+              },
+              child: const Icon(
+                Icons.cancel,
+                color: Colors.white,
+                size: 20,
+              ),
+            ))
+      ],
+    );
   }
 }
