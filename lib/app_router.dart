122b1098bb9620adbecd1c9f55bf99a6e40ab3a5
EugenAlexandrov <mybclym@gmail.com>
2022-10-13 22:04:57 +0300

    Создание задержки на сплеш скрине

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -5,8 +5,10 @@ import 'package:places/ui/screens/add_sight_screen.dart/filter_type_picker_scree
 import 'package:places/ui/screens/filters_screen.dart';
 import 'package:places/ui/screens/onboarding_screen.dart';
 import 'package:places/ui/screens/search_screen.dart';
+import 'package:places/ui/screens/splash_screen.dart';
 
 class AppRouter {
+  static const String splash = '/splash';
   static const String main = '/main_screen';
   static const String onboarding = '/onBoarding';
   static const String filters = '/main_screen/filters';
@@ -15,6 +17,7 @@ class AppRouter {
   static String searchScreen = '/main_screen/addSight/search';
 
   static Map<String, Widget Function(BuildContext context)> routes = {
+    AppRouter.splash: (_) => const SplashScreen(),
     AppRouter.main: (_) => const MainPage(),
     AppRouter.onboarding: (_) => const OnboardingScreen(),
     AppRouter.filters: (_) => const FiltersScreen(),
